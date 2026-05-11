5e257a992b4202727bf1b56fd7aa78490361e49a
Bret Zubieta <bzubietap@gmail.com>
2026-05-11 12:46:18 -0400

    BackUp

--- BootServer.java
+++ BootServer.java
@@ -10,7 +10,32 @@ public class BootServer extends BaseNode {
     }
 
     @Override
-    public void handleMessage (Messages msg) {
-        
+    public void handleEntry(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleEntry'");
+    }
+
+    @Override
+    public void handleExit(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleExit'");
+    }
+
+    @Override
+    public void handleInsert(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleInsert'");
+    }
+
+    @Override
+    public void handleDelete(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleDelete'");
+    }
+
+    @Override
+    public void handleLookUP(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleLookUP'");
     }
 }
--- NameServer.java
+++ NameServer.java
@@ -5,9 +5,34 @@ public class NameServer extends BaseNode {
     public void start() {
 
     }
+    
+    @Override
+    public void handleEntry(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleEntry'");
+    }
 
     @Override
-    public void handleMessage (Messages msg) {
-        
+    public void handleExit(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleExit'");
+    }
+
+    @Override
+    public void handleInsert(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleInsert'");
+    }
+
+    @Override
+    public void handleDelete(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleDelete'");
+    }
+
+    @Override
+    public void handleLookUP(Messages msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleLookUP'");
     }
 }
